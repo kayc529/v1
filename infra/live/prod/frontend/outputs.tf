5c5047962187f08c98c25689cdb03cb174889dcb
Kay Cheung <kaicipher966@gmail.com>
2025-08-31 22:40:10 -0700

    removed outputs.tf in frontend live stack

--- infra/live/prod/frontend/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "cloudfront_domain" { value = module.static_site.cloudfront_domain }
-output "cloudfront_id"     { value = module.static_site.cloudfront_id }
-output "bucket_name"       { value = module.static_site.bucket_name }
